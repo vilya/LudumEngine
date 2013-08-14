86e13ac05a04aec27a5fe6704756c86cf10a4da0
Vilya Harvey <vilya.harvey@gmail.com>
2013-08-14 23:28:05 +0100

    Add an asset loading class, ludum.Loader.
    This class does asynchronous loading of assets. Currently it only supports text
    assets (with an optional post-processing step which you can use to parse the
    text into something more useful).
    
    It supports grouping of assets and you can provide a post-processing callback
    to run when all assets in the group have finished loading. The use case for
    this was linking GLSL shaders when both the vertex and fragment shaders have
    been loaded & compiled. Note that assets cannot belong to more than one group
    (for now, at least).
    
    Progress is reported based only on the number of assets which have finished
    loading, rather than the number of bytes loaded, so it's pretty coarse grained.

--- src/ludum.js
+++ src/ludum.js
@@ -687,6 +687,190 @@ var ludum = function () {  // start of the ludum namespace
 
 
   //
+  // The Loader class
+  //
+
+  function Loader()
+  {
+    this.count = 0;
+    this.succeeded = 0;
+    this.failed = 0;
+    this.assets = {};
+    this.groups = {};
+    this.verbose = true;
+  }
+
+
+  Loader.prototype = {};
+
+
+  Loader.prototype.addGroup = function (name, postprocess)
+  {
+    if (this.groups[name])
+      return;
+
+    if (this.verbose)
+      console.log("adding asset group: " + name);
+
+    this.groups[name] = {
+      'assets': [],
+      'postprocess': postprocess,
+      'value': null
+    };
+  }
+
+
+  Loader.prototype.addText = function (url, /*optional*/ group, /*optional*/ postprocess)
+  {
+    if (this.assets[url])
+      return;
+
+    if (this.verbose)
+      console.log("adding text asset: " + url);
+
+    req = new XMLHttpRequest();
+    req.loader = this;
+    req.onload = function () { this.loader._onLoadText(url, this.responseText); }
+    req.onerror = function () { this.loader._onFailed(url, this.statusText); }
+    
+    this.count++;
+    this.assets[url] = {
+      'req': req,
+      'group': group,
+      'postprocess': postprocess,
+      'finished': false,
+      'value': null,
+      'error': null
+    };
+
+    if (group) {
+      if (!this.groups[group])
+        this.addGroup(group);
+      this.groups[group].assets.push(url);
+    }
+  }
+
+
+  Loader.prototype.start = function ()
+  {
+    if (this.verbose)
+      console.log("loading started");
+
+    for (var url in this.assets) {
+      var req = this.assets[url].req;
+      req.open("GET", url, true);
+      req.send();
+    }
+  }
+
+
+  Loader.prototype.finished = function ()
+  {
+    return (this.succeeded + this.failed) >= this.count;
+  }
+
+
+  Loader.prototype.groupFinished = function (groupName)
+  {
+    var group = this.groups[groupName];
+    if (!group)
+      return false;
+
+    for (var i = 0, end = group.assets.length; i < end; ++i) {
+      var asset = this.assets[group.assets[i]];
+      if (!asset.finished || asset.error)
+        return false;
+    }
+
+    return true;
+  }
+
+
+  Loader.prototype.fractionComplete = function ()
+  {
+    if (this.count == 0)
+      return 1.0;
+    else
+      return (this.succeeded + this.failed) / this.count;
+  }
+
+
+  Loader.prototype.fractionFailed = function ()
+  {
+    if (this.count == 0)
+      return 0.0;
+    else
+      return this.failed / this.count;
+  }
+
+
+  //
+  // Loader private methods
+  //
+
+  Loader.prototype._onLoadText = function (url, text)
+  {
+    var asset = this.assets[url];
+
+    asset.req = null;
+    asset.finished = true;
+
+    if (asset.postprocess)
+      asset.value = asset.postprocess(text);
+    else
+      asset.value = text;
+
+    if (asset.value) {
+      this.succeeded++;
+      if (this.verbose)
+        console.log("asset " + url + " loaded");
+      if (asset.group && this._canPostprocessGroup(asset.group))
+        this._postprocessGroup(asset.group);
+    }
+    else {
+      this._onFailed(url, "postprocessing failed");
+    }
+  }
+
+
+  Loader.prototype._onFailed = function (url, msg)
+  {
+    var asset = this.assets[url];
+    if (!asset)
+      return;
+
+    this.failed++;
+    asset.error = msg;
+    console.error(msg + " for " + url);
+  }
+
+
+  Loader.prototype._canPostprocessGroup = function (groupName)
+  {
+    var group = this.groups[groupName];
+    return group && group.postprocess && this.groupFinished(groupName);
+  }
+
+
+  Loader.prototype._postprocessGroup = function (groupName)
+  {
+    var group = this.groups[groupName];
+    if (!group || !group.postprocess)
+      return;
+
+    var args = [];
+    for (var i = 0, end = group.assets.length; i < end; ++i) {
+      var asset = this.assets[group.assets[i]];
+      args.push(asset.value);
+    }
+
+    group.value = group.postprocess.apply(undefined, args);
+    if (this.verbose)
+      console.log("finished postprocessing " + groupName);
+  }
+
+
+  //
   // Export public symbols.
   //
 
@@ -733,7 +917,9 @@ var ludum = function () {  // start of the ludum namespace
     // Browser functions
     'browserCapabilities': browserCapabilities,
     'showWarning': showWarning,
-    'showError': showError
+    'showError': showError,
+    // Asset loading
+    'Loader': Loader,
   };
 
 }(); // end of the ludum namespace.
