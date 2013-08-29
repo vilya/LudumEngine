e800ac5ee70bed10fa81334cd8a02d26ebe7f935
Vilya Harvey <vilya.harvey@gmail.com>
2013-08-29 13:02:47 +0100

    Test every function in src/base.js

--- tests/test-base.js
+++ tests/test-base.js
@@ -1,7 +1,3 @@
-// Include all the necessary ludum.min.js files before this.
-if (!ludum)
-  throw Error("you must include ludum.min.js before this file");
-
 test("base/hasNameClashes", function () {
   var a = { 'foo': true, 'bar': true };
   var b = { 'bar': true, 'baz': true };
@@ -28,3 +24,73 @@ test("base/copySymbols", function () {
   equal(a.foo, d.foo, "Changed the value for the clashing symbol.");
 });
 
+
+test("base/addSymbols", function () {
+  var exampleModule = {
+    'exampleSymbol1': function () { return true; },
+    'exampleSymbol2': function () { return false; },
+  };
+
+  ok(!ludum.hasNameClashes(exampleModule, ludum), "No symbols from exampleModule exist prior to addSymbols call.");
+  ludum.addSymbols(exampleModule);
+
+  var allSymbolsAdded = true;
+  var allSymbolsEqual = true;
+  for (var k in exampleModule) {
+    if (ludum[k] === undefined)
+      allSymbolsAdded = false;
+    if (ludum[k] !== exampleModule[k])
+      allSymbolsEqual = false;
+  }
+
+  ok(allSymbolsAdded, "All symbols from exampleModule added to ludum namespace.")
+  ok(allSymbolsEqual, "All symbols from exampleModule have the same value in the ludum namespace.");
+});
+
+
+test("base/addSubmodule", function () {
+  var exampleModule = {
+    'exampleSymbol1': function () { return true; },
+    'exampleSymbol2': function () { return false; },
+  };
+
+  equal(ludum.exampleModule, undefined, "ludum.exampleModule doesn't exist prior to addSubmodule call.");
+  ludum.addSubmodule("exampleModule", exampleModule);
+  equal(exampleModule, ludum.exampleModule, "ludum.exampleModule exists and has correct value after addSubmodule call.");
+});
+
+
+test("base/isValidIdentifier", function () {
+  var valid = [ "foo", "_foo", "Foo", "f", "F", "f0", "f_0", "f_", "_2" ];
+  var invalid = [ "200", "foo-bar", "foo+bar", "$foo" ];
+
+  for (var i = 0, end = valid.length; i < end; ++i)
+    ok(ludum.isValidIdentifier(valid[i], valid[i] + " is a valid identifier."));
+
+  for (var i = 0, end = invalid.length; i < end; ++i)
+    ok(!ludum.isValidIdentifier(invalid[i], invalid[i] + " is an invalid identifier."));
+});
+
+
+test("base/require", function () {
+  var exceptionThrown;
+  
+  try {
+    ludum.require("addSubmodule", "isValidIdentifier");
+    exceptionThrown = false;
+  }
+  catch (ex) {
+    exceptionThrown = true;
+  }
+  ok(!exceptionThrown, "No exception thrown when symbols do exist.");
+
+  equal(ludum.madeUpSymbol, undefined, "Test symbol doesn't exist before the require call.");
+  try {
+    ludum.require("", "isValidIdentifier");
+    exceptionThrown = false;
+  }
+  catch (ex) {
+    exceptionThrown = true;
+  }
+  ok(exceptionThrown, "Exception is thrown when symbols don't exist.");
+});
