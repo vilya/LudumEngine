371fa8667532cf1cb45a32f349ea1a78cd938d35
Vilya Harvey <vilya.harvey@gmail.com>
2013-08-29 12:15:38 +0100

    Add mising 'ludum.' before the hasNameClashes function in tests-base.js

--- tests/test-base.js
+++ tests/test-base.js
@@ -6,6 +6,7 @@ test("base/hasNameClashes", function () {
   var a = { 'foo': true, 'bar': true };
   var b = { 'bar': true, 'baz': true };
   var c = { 'baz': true, 'qux': true };
-  ok(hasNameClashes(a, b));
-  ok(!hasNameClashes(a, b));
+  ok(ludum.hasNameClashes(a, b));
+  ok(!ludum.hasNameClashes(a, c));
+  ok(ludum.hasNameClashes(b, c));
 });
