00a3e09ec289139d89f655d773c2f0f32b5d9f43
Vil <vilya.harvey@gmail.com>
2013-08-30 00:00:43 +0100

    Switch argument order for the equals() calls in the tests.
    The correct order is actual value then expected value; I had written them as
    expected value then actual value.

--- tests/test-math.js
+++ tests/test-math.js
@@ -2,61 +2,61 @@ module("math");
 
 
 test("radians", function () {
-  equal(0,                    ludum.radians(0), "0 degrees = 0 radians");
-
-  equal(Math.PI / 2.0,        ludum.radians(90.0), "90 degrees = PI/2 radians");
-  equal(Math.PI,              ludum.radians(180.0), "180 degrees = PI radians");
-  equal(Math.PI * 3.0 / 2.0,  ludum.radians(270.0), "270 degrees = 1.5 * PI radians");
-  equal(Math.PI * 2.0,        ludum.radians(360.0), "360 degrees = 2 * PI radians");
-
-  equal(-Math.PI / 2.0,       ludum.radians(-90), "-90 degrees = -PI/2 radians");
-  equal(-Math.PI,             ludum.radians(-180), "-180 degrees = -PI radians");
-  equal(-Math.PI * 3.0 / 2.0, ludum.radians(-270), "-270 degrees = -1.5 * PI radians");
-  equal(-Math.PI * 2.0,       ludum.radians(-360), "-360 degrees = -2 * PI radians");
+  equal(ludum.radians(0),     0,                    "0 degrees = 0 radians");
+                                                    
+  equal(ludum.radians(90.0),  Math.PI / 2.0,        "90 degrees = PI/2 radians");
+  equal(ludum.radians(180.0), Math.PI,              "180 degrees = PI radians");
+  equal(ludum.radians(270.0), Math.PI * 3.0 / 2.0,  "270 degrees = 1.5 * PI radians");
+  equal(ludum.radians(360.0), Math.PI * 2.0,        "360 degrees = 2 * PI radians");
+                                                    
+  equal(ludum.radians(-90),   -Math.PI / 2.0,       "-90 degrees = -PI/2 radians");
+  equal(ludum.radians(-180),  -Math.PI,             "-180 degrees = -PI radians");
+  equal(ludum.radians(-270),  -Math.PI * 3.0 / 2.0, "-270 degrees = -1.5 * PI radians");
+  equal(ludum.radians(-360),  -Math.PI * 2.0,       "-360 degrees = -2 * PI radians");
 });
 
 
 test("degrees", function () {
-  equal(0,    ludum.degrees(0),                     "0 radians = 0 degrees");
-
-  equal(90,   ludum.degrees(Math.PI / 2.0),         "PI/2 radians = 90 degrees");
-  equal(180,  ludum.degrees(Math.PI),               "PI radians = 180 degrees");
-  equal(270,  ludum.degrees(Math.PI * 3.0 / 2.0),   "1.5 * PI radians = 270 degrees");
-  equal(360,  ludum.degrees(Math.PI * 2.0),         "2 * PI radians = 360 degrees");
-
-  equal(-90,  ludum.degrees(-Math.PI / 2.0),        "-PI/2 radians = -90 degrees");
-  equal(-180, ludum.degrees(-Math.PI),              "-PI radians = -180 degrees");
-  equal(-270, ludum.degrees(-Math.PI * 3.0 / 2.0),  "-1.5 * PI radians = -270 degrees");
-  equal(-360, ludum.degrees(-Math.PI * 2.0),        "-2 * PI radians = -360 degrees");
+  equal(ludum.degrees(0),                     0,    "0 radians = 0 degrees");
+                                                    
+  equal(ludum.degrees(Math.PI / 2.0),         90,   "PI/2 radians = 90 degrees");
+  equal(ludum.degrees(Math.PI),               180,  "PI radians = 180 degrees");
+  equal(ludum.degrees(Math.PI * 3.0 / 2.0),   270,  "1.5 * PI radians = 270 degrees");
+  equal(ludum.degrees(Math.PI * 2.0),         360,  "2 * PI radians = 360 degrees");
+                                                    
+  equal(ludum.degrees(-Math.PI / 2.0),        -90,  "-PI/2 radians = -90 degrees");
+  equal(ludum.degrees(-Math.PI),              -180, "-PI radians = -180 degrees");
+  equal(ludum.degrees(-Math.PI * 3.0 / 2.0),  -270, "-1.5 * PI radians = -270 degrees");
+  equal(ludum.degrees(-Math.PI * 2.0),        -360, "-2 * PI radians = -360 degrees");
 });
 
 
 test("roundTo", function () {
-  equal("1.234",  ludum.roundTo(1.23432, 3),  "Rounds down when necessary.");
-  equal("1.235",  ludum.roundTo(1.23456, 3),  "Rounds up when necessary.");
-  equal("-1.234", ludum.roundTo(-1.23432, 3), "Rounds negative numbers down when necessary.");
-  equal("-1.235", ludum.roundTo(-1.23456, 3), "Rounds negative numbers up when necessary.");
-  equal("1",      ludum.roundTo(1.23456, 0),  "Produces an int when decimalPlaces = 0.");
-  equal("0.6",    ludum.roundTo(0.55, 1),     "Prefers to round away from zero, not towards odd digits.");
-  equal("0.7",    ludum.roundTo(0.65, 1),     "Prefers to round away from zero, not towards even digits.");
-  equal("-0.6",   ludum.roundTo(-0.55, 1),    "Prefers to round away from zero when negative, not towards even digits.");
-  equal("-0.7",   ludum.roundTo(-0.65, 1),    "Prefers to round away from zero when negative, not towards odd digits.");
+  equal(ludum.roundTo(1.23432, 3),  "1.234",  "Rounds down when necessary.");
+  equal(ludum.roundTo(1.23456, 3),  "1.235",  "Rounds up when necessary.");
+  equal(ludum.roundTo(-1.23432, 3), "-1.234", "Rounds negative numbers down when necessary.");
+  equal(ludum.roundTo(-1.23456, 3), "-1.235", "Rounds negative numbers up when necessary.");
+  equal(ludum.roundTo(1.23456, 0),  "1",      "Produces an int when decimalPlaces = 0.");
+  equal(ludum.roundTo(0.55, 1),     "0.6",    "Prefers to round away from zero, not towards odd digits.");
+  equal(ludum.roundTo(0.65, 1),     "0.7",    "Prefers to round away from zero, not towards even digits.");
+  equal(ludum.roundTo(-0.55, 1),    "-0.6",   "Prefers to round away from zero when negative, not towards even digits.");
+  equal(ludum.roundTo(-0.65, 1),    "-0.7",   "Prefers to round away from zero when negative, not towards odd digits.");
 });
 
 
 test("clamp", function () {
-  equal(2.9, ludum.clamp(2.9, 2.0, 4.0), "Value in range stays unchanged.");
-  equal(4.0, ludum.clamp(4.1, 2.0, 4.0), "Too-large value gets clamped to the top of the range.");
-  equal(2.0, ludum.clamp(1.7, 2.0, 4.0), "Too-small value gets clamped to the bottom of the range.");
-
-  equal(-2.9, ludum.clamp(-2.9, -4.0, -2.0), "Value in range stays unchanged even when negative.");
-  equal(-4.0, ludum.clamp(-4.1, -4.0, -2.0), "Too-large value gets clamped to the top of the range even when negative.");
-  equal(-2.0, ludum.clamp(-1.7, -4.0, -2.0), "Too-small value gets clamped to the bottom of the range even when negative.");
+  equal(ludum.clamp(2.9, 2.0, 4.0),      2.9, "Value in range stays unchanged.");
+  equal(ludum.clamp(4.1, 2.0, 4.0),      4.0, "Too-large value gets clamped to the top of the range.");
+  equal(ludum.clamp(1.7, 2.0, 4.0),      2.0, "Too-small value gets clamped to the bottom of the range.");
+                                              
+  equal(ludum.clamp(-2.9, -4.0, -2.0),  -2.9, "Value in range stays unchanged even when negative.");
+  equal(ludum.clamp(-4.1, -4.0, -2.0),  -4.0, "Too-large value gets clamped to the top of the range even when negative.");
+  equal(ludum.clamp(-1.7, -4.0, -2.0),  -2.0, "Too-small value gets clamped to the bottom of the range even when negative.");
 });
 
 
 test("saturate", function () {
-  equal(0.5, ludum.saturate(0.5),   "Value between 0.0 and 1.0 stays unchanged.");
-  equal(1.0, ludum.saturate(4.0),   "Value greater than 1.0 gets clamped to 1.0.");
-  equal(0.0, ludum.saturate(-1.7),  "Value less than 0.0 gets clamped to 0.0.");
+  equal(ludum.saturate(0.5),   0.5, "Value between 0.0 and 1.0 stays unchanged.");
+  equal(ludum.saturate(4.0),   1.0, "Value greater than 1.0 gets clamped to 1.0.");
+  equal(ludum.saturate(-1.7),  0.0, "Value less than 0.0 gets clamped to 0.0.");
 });
--- tests/test-states.js
+++ tests/test-states.js
@@ -2,13 +2,13 @@ module("states");
 
 test("StateMachine constructor", function () {
   var machine = new ludum.StateMachine();
-  deepEqual({}, machine.contextDefaults, "No-arg constructor creates empty contextDefaults.");
-  equal(0, machine.initialState, "No-arg constructor sets initial state to zero.");
+  deepEqual(machine.contextDefaults, {}, "No-arg constructor creates empty contextDefaults.");
+  equal(machine.initialState, 0, "No-arg constructor sets initial state to zero.");
 
   var contextDefaults = { 'foo': 'bar' };
   machine = new ludum.StateMachine(contextDefaults);
-  deepEqual(contextDefaults, machine.contextDefaults, "1-arg constructor sets contextDefaults correctly.");
-  equal(0, machine.initialState, "1-arg constructor sets initial state to zero.");
+  deepEqual(machine.contextDefaults, contextDefaults, "1-arg constructor sets contextDefaults correctly.");
+  equal(machine.initialState, 0, "1-arg constructor sets initial state to zero.");
 
   var badContextDefaults = { 'currentState': 10 };
   throws(function () { new ludum.StateMachine(badContextDefaults); }, "Throws when contextDefaults has a name clash with our internal state-tracking symbols.");
@@ -17,11 +17,11 @@ test("StateMachine constructor", function () {
 
 test("StateMachine.setContextDefaults", function () {
   var machine = new ludum.StateMachine();
-  deepEqual({}, machine.contextDefaults, "contextDefaults are an empty object before the setContextDefaults call.");
+  deepEqual(machine.contextDefaults, {}, "contextDefaults are an empty object before the setContextDefaults call.");
 
   var contextDefaults = { 'foo': 'bar' };
   machine.setContextDefaults(contextDefaults);
-  deepEqual(contextDefaults, machine.contextDefaults, "contextDefaults are set correctly when they don't contain name clashes.");
+  deepEqual(machine.contextDefaults, contextDefaults, "contextDefaults are set correctly when they don't contain name clashes.");
 
   var badContextDefaults = { 'currentState': 10 };
   throws(function () { machine.setContextDefaults(badContextDefaults); }, "Throws when contextDefaults has a name clash with our internal state-tracking symbols.");
@@ -30,7 +30,7 @@ test("StateMachine.setContextDefaults", function () {
 
 test("StateMachine.setInitialState", function () {
   var machine = new ludum.StateMachine();
-  equal(0, machine.initialState, "Initial state defaults to zero.");
+  equal(machine.initialState, 0, "Initial state defaults to zero.");
   throws(function () { machine.setInitialState(1); }, "Throws if you try to set the state to a number which hasn't been added yet.");
   throws(function () { machine.setInitialState(0); }, "Throws if you try to set the state to zero, even if it's already zero.");
 
@@ -38,29 +38,29 @@ test("StateMachine.setInitialState", function () {
   machine.addState('BAR');
 
   machine.setInitialState(machine.BAR);
-  equal(machine.BAR, machine.initialState, "Initial state is set to BAR.");
+  equal(machine.initialState, machine.BAR, "Initial state is set to BAR.");
   machine.setInitialState(machine.FOO);
-  equal(machine.FOO, machine.initialState, "Initial state is set back to FOO.");
+  equal(machine.initialState, machine.FOO, "Initial state is set back to FOO.");
 });
 
 
 test("StateMachine.addState", function () {
   var machine = new ludum.StateMachine();
-  equal(0, machine.states.length, "Start with no states.");
+  equal(machine.states.length, 0, "Start with no states.");
 
   machine.addState('FOO');
-  equal(1, machine.states.length, "One state added.");
+  equal(machine.states.length, 1, "One state added.");
   ok(machine.FOO !== undefined, "A constant for the new state has been added to the state machine.");
-  equal('FOO', machine.states[machine.FOO].name, "State added with the correct name.");
-  deepEqual([], machine.states[machine.FOO].transitions, "State added with empty list of transitions.");
+  equal(machine.states[machine.FOO].name, 'FOO', "State added with the correct name.");
+  deepEqual(machine.states[machine.FOO].transitions, [], "State added with empty list of transitions.");
 
   throws(function () { machine.addState('FOO'); }, "Throws if the state is already defined.");
   throws(function () { machine.addState("invalid state"); }, "Throws if you try to add a state with a name that isn't a valid identifier.");
   throws(function () { machine.addState("BAR", { 'name': 'bar' }); }, "Throws if the stateTemplate contains called 'name' or 'transitions'.");
 
   machine.addState('BAR', { 'treasures': 10 });
-  equal(2, machine.states.length, "Second state added.");
+  equal(machine.states.length, 2, "Second state added.");
   ok(machine.BAR !== undefined, "A constant for the new state has been added to the state machine.");
-  equal('BAR', machine.states[machine.BAR].name, "State added with the correct name.");
-  equal(10, machine.states[machine.BAR].treasures, "Extra variable copied from stateTemplate.");
+  equal(machine.states[machine.BAR].name, 'BAR', "State added with the correct name.");
+  equal(machine.states[machine.BAR].treasures, 10, "Extra variable copied from stateTemplate.");
 });
